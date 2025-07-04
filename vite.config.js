e99f09a89978d85390d3d1ae3268d5ab3373053e
Umesh Dev <umeshhgupta.dev@gmail.com>
2025-07-04 15:51:00 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sign-up-log-in/',
+  base: '/signup-page/',
   plugins: [react()],
 })
